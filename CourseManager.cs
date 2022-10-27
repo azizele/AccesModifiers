c4bc33c754c5f8aeb7c5c7f80743ff964b3da75c
Coder Club <hi@coderclub.com>
2022-10-28 00:39:55 +0300

    AccesModifiersAdded!

---
+++ CourseManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    
+    class CourseManager
+    {
+        public string MyProperty { get; set; }
+        private class MyClass
+        {
+
+        }
+    }
+}
